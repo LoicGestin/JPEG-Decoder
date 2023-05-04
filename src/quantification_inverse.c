f0b8f6ea6f9bc5179e3aec6d879ce15be097bcd1
mevels <sarah.mevel@grenoble-inp.org>
2023-05-04 17:02:03 +0200

    modification quantification

--- src/quantification_inverse.c
+++ src/quantification_inverse.c
@@ -1,21 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int ** quantification_inverse(int **frequentiel, int *quantification){
+int * quantification_inverse(int **frequentiel, int *quantification){
 
-    int **matrice = malloc(8*sizeof(int*));
-    for(int i=0; i<8; i++){
-        matrice[i]=malloc(8*sizeof(int));
-    }
+    int *table = malloc(64*sizeof(int*));
     int k=0;
     for(int i=0; i<8; i++){
         for(int j=0; j<8; j++){
-            matrice[i][j]= frequentiel[i][j]*quantification[k];
+            table[k]= frequentiel[i][j]*quantification[k];
             k++;  
         }
     }
     
-    return matrice; 
+    return table; 
 }
 
 int   main(){
@@ -32,13 +29,12 @@ int   main(){
     for(int i=0; i<64; i++){
         quantification[i]=i;
     }
-    int **matrice=quantification_inverse(fréquentiel, quantification);
-     for(int i = 0; i < 8; i ++){
-        for(int j = 0; j < 8; j ++){
-            printf("%04d ",matrice[i][j]);
+    int *table=quantification_inverse(fréquentiel, quantification);
+     for(int i = 0; i < 64; i ++){
+        printf("%03d ",table[i]);
         }
         printf("\n");
-    }
+    
     return 0;
 
 }
