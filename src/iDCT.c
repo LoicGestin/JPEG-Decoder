f2059890f558178994b7cb75747e213d3df0e6e4
Sarah Mevel <sarah.mevel@grenoble-inp.org>
2023-05-17 15:30:53 +0000

    Update iDCT.c

--- src/iDCT.c
+++ src/iDCT.c
@@ -5,7 +5,7 @@
 
 #include "../include/iDCT.h"
 
-
+#define pi 3.14159265359
 
 void iDCT(int16_t **phi, uint8_t **S, float cos_tab[8][8]){
     /*Fonction réalisant l'inverse de la transformée en cosinus discrète à partir
@@ -15,7 +15,9 @@ void iDCT(int16_t **phi, uint8_t **S, float cos_tab[8][8]){
     float dep = (1 / sqrt(16));
     
     float racine = sqrt(2);
-
+    float inverse_racine = 1 / racine;
+    float cos_16 = cosf(pi/16);
+    float sin_16 = sinf(pi/16);
     // Moitié du coefficient DC
     float t1 =  (float)phi[0][0] / 2;
 
@@ -27,7 +29,47 @@ void iDCT(int16_t **phi, uint8_t **S, float cos_tab[8][8]){
 
     // Parcours des éléments de la matrice S
     for(int8_t x=0; x<8; x++){
+        /*
+        i0 = phi[x][0];
+        i4 = phi[x][4];
+        i2 = phi[x][2];
+        i6 = phi[x][6];
+        i7 = (phi[x][1] - phi[x][7])/2;
+        i1 = (phi[x][1] + phi[x][7])/2;
+        i3 = (1/racine)*phi[x][3]; 
+        i5 = (1/racine)*phi[x][5];*/
 
+
+        float l0 = (phi[x][0]+ phi[x][4])/2;
+        float l4 = (phi[x][0]- phi[x][4])/2;
+        float l2 = phi[x][2]*inverse_racine*cos_16 - phi[x][6] * inverse_racine*sin_16;
+        float l6 = phi[x][6]*inverse_racine*cos_16 + phi[x][2] * inverse_racine*sin_16;
+        float l7 = ((phi[x][1] - phi[x][7])/2 + (phi[x][5]*inverse_racine))/2;
+        float l1 = ((phi[x][1] + phi[x][7])/2 - inverse_racine*phi[x][3])/2;
+        float l3 = ((phi[x][1] - phi[x][7])/2 - (phi[x][5]*inverse_racine))/2;
+        float l5 = (inverse_racine*phi[x][3] + (phi[x][1] + phi[x][7])/2)/2;
+
+        float ll0 = (l0 + l6)/2;
+        float ll4 = (l4 + l2)/2;
+        float ll2 = (l4 - l2)/2;
+        float ll6 = (l0 - l6)/2;
+        float ll7 = l7*cos_16 - l1*sin_16;
+        float ll1 = l1*cos_16 + l7*sin_16;
+        float ll3 = l3*cos_16 - l5*sin_16;
+        float ll5 = l5*cos_16 - l3*sin_16;
+
+  
+
+        
+        S[x][0] = (uint8_t)round(fmaxf(0, fminf(255, (ll0 + ll1)*8+128)));
+        S[x][4] = (uint8_t)round(fmaxf(0, fminf(255, (ll4 + ll5)*8+128))); 
+        S[x][2] = (uint8_t)round(fmaxf(0, fminf(255, (ll2 + ll3)*8+128))); 
+        S[x][6] = (uint8_t)round(fmaxf(0, fminf(255, (ll6 + ll7)*8+128))); 
+        S[x][7] = (uint8_t)round(fmaxf(0, fminf(255, (ll6 - ll7)*8+128)));  
+        S[x][1] = (uint8_t)round(fmaxf(0, fminf(255, (ll0 - ll1)*8+128)));  
+        S[x][3] = (uint8_t)round(fmaxf(0, fminf(255, (ll2 - ll3)*8+128)));  
+        S[x][5] = (uint8_t)round(fmaxf(0, fminf(255, (ll4 - ll5)*8+128))); 
+        /*
         for(int8_t y=0; y<8; y++){
             
             // Calcul de t2 grâce à une somme sur mu
@@ -54,6 +96,6 @@ void iDCT(int16_t **phi, uint8_t **S, float cos_tab[8][8]){
             float s = dep * (t1 + t2 + t3 + t4);
             S[x][y] = (uint8_t)round(fmaxf(0, fminf(255, s + 128)));
            
-        }
+        }*/
     }
 }
