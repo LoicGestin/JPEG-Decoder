cee3dd0c7876fd84d30e48c06275ff5e1706663d
Thomas Messina <thomas.messina@grenoble-inp.org>
2023-05-17 05:46:22 +0000

    Update quantification_inverse.c

--- src/quantification_inverse.c
+++ src/quantification_inverse.c
@@ -6,13 +6,11 @@
 
 void quantification_inverse(struct data *d, int8_t index,  int16_t *frequentiel){
     /*Fonction prenant en entrée un pointeur vers notre structure data, un index
-    et un pointeur vers et un pointeur vers le bloc fréquentiel et effectuant
-    la multiplication élément par élément de la table de quantification par le
-    bloc fréquentiel. On retournera le tableau obtenu*/
+    et un pointeur vers le bloc fréquentiel et effectuantla multiplication élément
+    par élément de la table de quantification par le bloc fréquentiel*/
     int16_t *quantification = d->quantization_table_read[index];
     for(int8_t i=0; i<64; i++){
         frequentiel[i]= frequentiel[i]*quantification[i];
     }
-
 }
 
