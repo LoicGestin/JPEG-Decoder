6f1ec42f73257623741db4a72f34e5d863ac3de4
Thomas Messina <thomas.messina@grenoble-inp.org>
2023-05-17 15:34:25 +0000

    Update couleur_matrice.h

--- include/couleur_matrice.h
+++ include/couleur_matrice.h
@@ -7,8 +7,9 @@
 #include <stdint.h>
 #include "decode_entete.h"
 
-void YCbCr_to_R(uint8_t **Y, uint8_t **Cb, uint8_t **Cr, struct data *d, uint8_t **R);
-void YCbCr_to_B(uint8_t **Y, uint8_t **Cb, uint8_t **Cr, struct data *d, uint8_t **B);
+void YCbCr_to_R(uint8_t **Y, uint8_t **Cr, struct data *d, uint8_t **R);
 void YCbCr_to_G(uint8_t **Y, uint8_t **Cb, uint8_t **Cr, struct data *d, uint8_t **G);
+void YCbCr_to_B(uint8_t **Y, uint8_t **Cb, struct data *d, uint8_t **B);
+
 
 #endif /* _COULEUR_MATRICE_H_ */
