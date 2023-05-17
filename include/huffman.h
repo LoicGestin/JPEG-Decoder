adb7fda22f64eb117ca840bf987ca435c016e36c
Thomas Messina <thomas.messina@grenoble-inp.org>
2023-05-17 15:36:10 +0000

    Update huffman.h

--- include/huffman.h
+++ include/huffman.h
@@ -5,7 +5,7 @@
 
 void insert_code_huffman(struct cellule_huffman **c,int16_t value, int16_t length, int16_t symbol);
 
-void decode_huffman(struct dht_ac_dc *current_dht, int8_t table_type);
+void decode_huffman(struct dht_ac_dc *current_dht);
 
 void display_huffman_tree(struct cellule_huffman* c, char* path);
 
