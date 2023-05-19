ef9b8a5d1c95c6d4373381fb06e68c72bd3e4ddb
loic gestin <loic.gestin@grenoble-inp.org>
2023-05-19 20:16:50 +0200

    eee

--- src/iDCT.c
+++ src/iDCT.c
@@ -16,8 +16,12 @@ void iDCT(int16_t **phi, uint8_t **S, float cos_tab[8][8]){
     
     float racine = sqrt(2);
     float inverse_racine = 1 / racine;
-    float cos_16 = cosf(pi/16);
-    float sin_16 = sinf(pi/16);
+    float cos6_16 = cosf((6 *pi)/16);
+    float sin6_16 = sinf((6 *pi)/16);
+    float cos3_16 = cosf((3 *pi)/16);
+    float sin3_16 = sinf((3 *pi)/16);
+    float cos1_16 = cosf((1 *pi)/16);
+    float sin1_16 = sinf((1 *pi)/16);
     // Moitié du coefficient DC
     float t1 =  (float)phi[0][0] / 2;
 
@@ -29,65 +33,90 @@ void iDCT(int16_t **phi, uint8_t **S, float cos_tab[8][8]){
 
     // Parcours des éléments de la matrice S
       for(int8_t x=0; x<8; x++){
+        
+          
+        float l0 = phi[x][0];
+        float l4 = phi[x][1] - phi[x][7];
+        float l2 = phi[x][2];
+        float l6 = phi[x][5] * racine;
+        float l7 = phi[x][1] + phi[x][7];
+        float l3 = phi[x][6];
+        float l5 = phi[x][3] * racine;
+        float l1 = phi[x][4];
 
-        float l0 = (phi[x][0]+ phi[x][4])/2;
-        float l4 = (phi[x][0]- phi[x][4])/2;
-        float l2 = phi[x][2]*inverse_racine*cos_16 - phi[x][6] * inverse_racine*sin_16;
-        float l6 = phi[x][6]*inverse_racine*cos_16 + phi[x][2] * inverse_racine*sin_16;
-        float l7 = ((phi[x][1] - phi[x][7])/2 + (phi[x][5]*inverse_racine))/2;
-        float l1 = ((phi[x][1] + phi[x][7])/2 - inverse_racine*phi[x][3])/2;
-        float l3 = ((phi[x][1] - phi[x][7])/2 - (phi[x][5]*inverse_racine))/2;
-        float l5 = (inverse_racine*phi[x][3] + (phi[x][1] + phi[x][7])/2)/2;
+         
+        float ll0 = l0 + l1;
+        float ll1 = l0 - l1;
+        float ll2 =  l2*racine*cos6_16 - l3* racine*sin6_16;
+        float ll3 =  l3*racine*cos6_16 + l0* racine*sin6_16;
+        float ll4 = l4 + l6;
+        float ll5 = l7 - l5;
+        float ll6 = l4 - l6;
+        float ll7 = l7 + l5;
+        
+        
+        l0 = ll0 + ll3;
+        l1 = ll1 + ll2;
+        l2 = ll1 - ll2;
+        l3 = ll0 - ll3;
+        l4 = l4 * cos3_16 - l7 * sin3_16;
+        l5 = l5 * cos1_16 - l6 * sin1_16;
+        l6 = l6 * cos1_16 + l5 * sin1_16;
+        l7 = l7 * cos3_16 + l4 * sin3_16;
 
-        float ll0 = (l0 + l6)/2;
-        float ll4 = (l4 + l2)/2;
-        float ll2 = (l4 - l2)/2;
-        float ll6 = (l0 - l6)/2;
-        float ll7 = l7*cos_16 - l1*sin_16;
-        float ll1 = l1*cos_16 + l7*sin_16;
-        float ll3 = l3*cos_16 - l5*sin_16;
-        float ll5 = l5*cos_16 - l3*sin_16;
+        tmp[x][0] = l0 + l7;
+        tmp[x][1] = l1 + l6; 
+        tmp[x][2] = l2 + l5; 
+        tmp[x][3] = l3 + l4; 
+        tmp[x][4] = l3 - l4;  
+        tmp[x][5] = l2 - l5;  
+        tmp[x][6] = l1 - l6;  
+        tmp[x][7] = l0 - l7; 
 
-    
-        tmp[x][0] = ll0 + ll1;
-        tmp[x][4] = ll4 + ll5; 
-        tmp[x][2] = ll2 + ll3; 
-        tmp[x][6] = ll6 + ll7; 
-        tmp[x][7] = ll6 - ll7;  
-        tmp[x][1] = ll0 - ll1;  
-        tmp[x][3] = ll2 - ll3;  
-        tmp[x][5] = ll4 - ll5; 
+        
+        
      
     }
     for(int8_t x=0; x<8; x++){
 
-        float l0 = (tmp[0][x]+ tmp[4][x])/2;
-        float l4 = (tmp[0][x]- tmp[4][x])/2;
-        float l2 = tmp[2][x]*inverse_racine*cos_16 - tmp[6][x]* inverse_racine*sin_16;
-        float l6 = tmp[6][x]*inverse_racine*cos_16 + tmp[2][x] * inverse_racine*sin_16;
-        float l7 = ((tmp[1][x] - tmp[7][x])/2 + (tmp[5][x]*inverse_racine))/2;
-        float l1 = ((tmp[1][x] + tmp[7][x])/2 - inverse_racine*tmp[3][x])/2;
-        float l3 = ((tmp[1][x] - tmp[7][x])/2 - (tmp[5][x]*inverse_racine))/2;
-        float l5 = (inverse_racine*tmp[3][x] + (tmp[1][x] + tmp[7][x])/2)/2;
+           
+        float l0 = tmp[0][x];
+        float l4 = tmp[1][x] - tmp[7][x];
+        float l2 = tmp[2][x];
+        float l6 = tmp[5][x] * racine;
+        float l7 = tmp[1][x] + tmp[7][x];
+        float l3 = tmp[6][x];
+        float l5 = tmp[3][x] * racine;
+        float l1 = tmp[4][x];
 
-        float ll0 = (l0 + l6)/2;
-        float ll4 = (l4 + l2)/2;
-        float ll2 = (l4 - l2)/2;
-        float ll6 = (l0 - l6)/2;
-        float ll7 = l7*cos_16 - l1*sin_16;
-        float ll1 = l1*cos_16 + l7*sin_16;
-        float ll3 = l3*cos_16 - l5*sin_16;
-        float ll5 = l5*cos_16 - l3*sin_16;
-
-    
-        S[0][x] = (uint8_t)round(fmaxf(0, fminf(255, (ll0 + ll1)*2+128)));
-        S[4][x] = (uint8_t)round(fmaxf(0, fminf(255, (ll4 + ll5)*2+128))); 
-        S[2][x] = (uint8_t)round(fmaxf(0, fminf(255, (ll2 + ll3)*2+128))); 
-        S[6][x] = (uint8_t)round(fmaxf(0, fminf(255, (ll6 + ll7)*2+128))); 
-        S[7][x] = (uint8_t)round(fmaxf(0, fminf(255, (ll6 - ll7)*2+128)));  
-        S[1][x] = (uint8_t)round(fmaxf(0, fminf(255, (ll0 - ll1)*2+128)));  
-        S[3][x] = (uint8_t)round(fmaxf(0, fminf(255, (ll2 - ll3)*2+128)));  
-        S[5][x] = (uint8_t)round(fmaxf(0, fminf(255, (ll4 - ll5)*2+128))); 
+         
+        float ll0 = l0 + l1;
+        float ll1 = l0 - l1;
+        float ll2 =  l2*racine*cos6_16 - l3* racine*sin6_16;
+        float ll3 =  l3*racine*cos6_16 + l0* racine*sin6_16;
+        float ll4 = l4 + l6;
+        float ll5 = l7 - l5;
+        float ll6 = l4 - l6;
+        float ll7 = l7 + l5;
+        
+        
+        l0 = ll0 + ll3;
+        l1 = ll1 + ll2;
+        l2 = ll1 - ll2;
+        l3 = ll0 - ll3;
+        l4 = l4 * cos3_16 - l7 * sin3_16;
+        l5 = l5 * cos1_16 - l6 * sin1_16;
+        l6 = l6 * cos1_16 + l5 * sin1_16;
+        l7 = l7 * cos3_16 + l4 * sin3_16;
+ 
+        S[0][x] = (uint8_t)round(fmaxf(0, fminf(255, (l0 + l7)/8+128)));
+        S[1][x] = (uint8_t)round(fmaxf(0, fminf(255, (l1 + l6)/8+128))); 
+        S[2][x] = (uint8_t)round(fmaxf(0, fminf(255, (l2 + l5)/8+128))); 
+        S[3][x] = (uint8_t)round(fmaxf(0, fminf(255, (l3 + l4)/8+128))); 
+        S[4][x] = (uint8_t)round(fmaxf(0, fminf(255, (l3 - l4)/8+128)));  
+        S[5][x] = (uint8_t)round(fmaxf(0, fminf(255, (l2 - l5)/8+128)));  
+        S[6][x] = (uint8_t)round(fmaxf(0, fminf(255, (l1 - l6)/8+128)));  
+        S[7][x] = (uint8_t)round(fmaxf(0, fminf(255, (l0 - l7)/8+128))); 
      
     }
 }
