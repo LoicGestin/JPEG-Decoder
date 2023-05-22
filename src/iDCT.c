2204b3f7bfee171dfde19ac006a4d029c8d3184a
Sarah Mevel <sarah.mevel@grenoble-inp.org>
2023-05-22 07:49:45 +0000

    Update iDCT.c

--- src/iDCT.c
+++ src/iDCT.c
@@ -6,30 +6,19 @@
 #include "../include/iDCT.h"
 
 #define pi 3.14159265359
+#define inverse_racine = 1 / sqrt(2);
+#define cos6_16  cosf((6 *pi)/16);
+#define sin6_16  sinf((6 *pi)/16);
+#define cos3_16  cosf((3 *pi)/16);
+#define sin3_16  sinf((3 *pi)/16);
+#define cos1_16  cosf((1 *pi)/16);
+#define sin1_16  sinf((1 *pi)/16);
 
 
 
 void iDCT_rapide(int16_t **phi, uint8_t **S ){
     /*Fonction réalisant l'inverse de la transformée en cosinus discrète à partir
     d'une matrice phi 8x8 et stockant le résultat dans une matrice S*/
-
-    // dep = (1 / sqrt(16)) car n = 8
-    float dep = (1 / sqrt(16));
-    
-    float racine = sqrt(2);
-    float inverse_racine = 1 / racine;
-    float cos6_16 = cosf((6 *pi)/16);
-    float sin6_16 = sinf((6 *pi)/16);
-    float cos3_16 = cosf((3 *pi)/16);
-    float sin3_16 = sinf((3 *pi)/16);
-    float cos1_16 = cosf((1 *pi)/16);
-    float sin1_16 = sinf((1 *pi)/16);
-    // Moitié du coefficient DC
-    float t1 =  (float)phi[0][0] / 2;
-
-    // sortirde iDCT
-    // Pré calcule des cosinus pour réduire le nombre de calcul et rendre le code plus rapide
-    // Création d'un tableau contenant tous les valuers des cosinus
    
     int16_t tmp[8][8];
 
@@ -120,4 +109,55 @@ void iDCT_rapide(int16_t **phi, uint8_t **S ){
         S[7][x] = (uint8_t)round(fmaxf(0, fminf(255, (l0 - l7)/8+128))); 
      
     }
+}
+void iDCT(int16_t **phi, uint8_t **S, float cos_tab[8][8]){
+    /*Fonction réalisant l'inverse de la transformée en cosinus discrète à partir
+    d'une matrice phi 8x8 et stockant le résultat dans une matrice S*/
+
+    float cos_table[8][8];
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            cos_table[i][j] = cosf(((2*j+1)*i*pi)/(16));
+        }
+    }
+
+    // dep = (1 / sqrt(16)) car n = 8
+    float dep = (1 / sqrt(16));
+            
+    float racine = sqrt(2);
+
+    // Moitié du coefficient DC
+    float t1 =  (float)phi[0][0] / 2;
+
+    // Parcours des éléments de la matrice S
+    for(int8_t x=0; x<8; x++){
+
+        for(int8_t y=0; y<8; y++){
+
+            // Calcul de t2 grâce à une somme sur mu
+            float t2 = 0.0;
+            for (int8_t mu = 1; mu<8; mu++){
+                t2 += (float) (cos_table[mu][y] * (float) phi[0][mu])/racine;
+            }
+
+            // Calcul de t3 en utilisant une somme sur lambda
+            float t3 = 0.0;
+            for (int lambda = 1; lambda<8; lambda++){
+                t3 += ((float) cos_table[lambda][x] *  (float)phi[lambda][0])/racine;
+            }
+
+            // Calcul de t4 en effectuant une double somme sur lambda et mu
+            float t4 = 0.0;
+            for (int8_t lambda = 1; lambda<8; lambda++){
+                for (int8_t mu = 1; mu<8; mu++){
+                    t4 += (float) cos_table[mu][y] * cos_table[lambda][x] *  (float)phi[lambda][mu];
+                }
+            }
+      
+            // Regroupement des termes et stockage des valeurs
+            float s = dep * (t1 + t2 + t3 + t4);
+            S[x][y] = (uint8_t)round(fmaxf(0, fminf(255, s + 128)));
+           
+        }
+    } 
 }
