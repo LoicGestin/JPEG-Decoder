0dc86afcfacfcf4f2e2694fdddbd334676383fe6
loic gestin <loic.gestin@grenoble-inp.org>
2023-05-23 15:08:40 +0200

    couleur

--- src/couleur_matrice.c
+++ src/couleur_matrice.c
@@ -4,10 +4,7 @@
 
 #include "../include/couleur_matrice.h"
 #include "../include/decode_entete.h"
-
-void YCbCr_to_R(uint8_t **Y, uint8_t **Cr, struct data *d, uint8_t **R){
-    /*Fonction effectuant la conversion YCbCr to RGB pour la couleur rouge*/
-
+void YCbCr_to_RGB(uint8_t **Y, uint8_t **Cb, uint8_t **Cr, struct data *d, uint8_t **R, uint8_t **G, uint8_t **B){
     // Récupération des facteurs d'échantillonnages
     struct component *comp = d->list_component;
     int8_t sampling_w = comp->sampling_horizontal;
@@ -16,77 +13,19 @@ void YCbCr_to_R(uint8_t **Y, uint8_t **Cr, struct data *d, uint8_t **R){
     // Remplissage de la matrice
     for(uint8_t i=0; i<8*sampling_h; i++){
         for(uint8_t j=0; j<8*sampling_w; j++){
-            //float r = Y[i][j] - 0.0009267 * (Cb[i][j] - 128) + 1.4016868 * (Cr[i][j] - 128);
-            float r = Y[i][j] + 1.402 * (Cr[i][j] - 128);
-            // Troncature si les valeurs sont négatives ou supérieures à 255
-            if(r < 0){
-                R[i][j] = 0;
-            }
-            else if(r > 255){
-                R[i][j] = 255;
-            }
-            else{
-                R[i][j] = (uint8_t) r;
-            }
 
-        }
-    }
-    
-}
-
-void YCbCr_to_B(uint8_t **Y, uint8_t **Cb, struct data *d, uint8_t **B){
-    /*Fonction effectuant la conversion YCbCr to RGB pour la couleur bleue*/
-
-    // Récupération des facteurs d'échantillonnages
-    struct component *comp = d->list_component;
-    int8_t sampling_w = comp->sampling_horizontal;
-    int8_t sampling_h = comp->sampling_vertical;
-    
-    // Remplissage de la matrice
-    for(uint8_t i=0; i<8*sampling_h; i++){
-        for(uint8_t j=0; j<8*sampling_w; j++){
-            //float b  = Y[i][j] + 1.7721604 * (Cb[i][j] - 128) + 0.0009902 * (Cr[i][j] - 128);
-            float b = Y[i][j] + 1.772 * (Cb[i][j] - 128);
-            // Troncature si les valeurs sont négatives ou supérieures à 255
-            if(b < 0){
-                B[i][j] = 0;
-            }
-            else if(b > 255){
-                B[i][j] = 255;
-            }
-            else{
-                B[i][j] = (uint8_t) b;
-            }
+            // Pré-calcul des valeurs Cb et Cr ajustées
+            float Cb_adj = Cb[i][j] - 128;
+            float Cr_adj = Cr[i][j] - 128;
             
-        }
-    }
-    
-}
+            float r = Y[i][j] + 1.402 * Cr_adj;
+            float b = Y[i][j] + 1.772 * Cb_adj;
+            float g = Y[i][j] - 0.34414 * Cb_adj - 0.71414 * Cr_adj;
 
-void YCbCr_to_G(uint8_t **Y, uint8_t **Cb, uint8_t **Cr, struct data *d, uint8_t **G){
-    /*Fonction effectuant la conversion YCbCr to RGB pour la couleur verte*/
-
-    // Récupération des facteurs d'échantillonnages
-    struct component *comp = d->list_component;
-    int8_t sampling_w = comp->sampling_horizontal;
-    int8_t sampling_h = comp->sampling_vertical;
-    
-    // Remplissage de la matrice
-    for(uint8_t i=0; i<8*sampling_h; i++){
-        for(uint8_t j=0; j<8*sampling_w; j++){
-           //float g = Y[i][j] - 0.3436954 * (Cb[i][j] - 128) - 0.7141690 * (Cr[i][j] - 128);
-           float g = Y[i][j] - 0.34414 * (Cb[i][j] - 128) - 0.71414 * (Cr[i][j] - 128);
             // Troncature si les valeurs sont négatives ou supérieures à 255
-           if(g < 0){
-                G[i][j] = 0;
-            }
-            else if(g > 255){
-                G[i][j] = 255;
-            }
-            else{
-                G[i][j] = (uint8_t)g;
-            }
+            R[i][j] = r < 0 ?  0 : r > 255 ?  255 : (uint8_t) r;
+            G[i][j] = g < 0 ?  0 : g > 255 ?  255 : (uint8_t) g;
+            B[i][j] = b < 0 ?  0 : b > 255 ?  255 : (uint8_t) b;
         }
     }
-   
 }
